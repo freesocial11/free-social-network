bc2fead983c1fe1b0ddb5f5cbbe9ac8a7b6e6273
anshul <anshul@apexol.com>
2016-08-10 20:30:42 +0530

    Get users admin client template implemented

---
+++ Presentation/mobSocial.WebApp/app/admin/users/userService.js
@@ -0,0 +1,9 @@
+﻿window.mobSocial.service("userService", ["globalApiEndPoint", "webClientService", "$http", function (globalApiEndPoint, webClientService, $http) {
+
+    var apiEndPoint = globalApiEndPoint + "/users";
+    // get
+    this.get = function (userGetModel, success, error) {
+        webClientService.get(apiEndPoint + "/get", userGetModel, success, error);
+    }
+
+}]);
