dc1c5006941b33229af1f901ffa873387a2bd50f
anshul <anshul@apexol.com>
2016-08-10 20:29:50 +0530

    Get users page setup

--- Presentation/mobSocial.WebApi/Controllers/UserController.cs
+++ Presentation/mobSocial.WebApi/Controllers/UserController.cs
@@ -1,7 +1,13 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
 using mobSocial.Services.Users;
 using mobSocial.WebApi.Configuration.Infrastructure;
 using mobSocial.WebApi.Configuration.Mvc;
+using mobSocial.WebApi.Configuration.Security.Attributes;
+using mobSocial.WebApi.Extensions.ModelExtensions;
+using mobSocial.WebApi.Models.Users;
 
 namespace mobSocial.WebApi.Controllers
 {
@@ -16,11 +22,25 @@ namespace mobSocial.WebApi.Controllers
         }
 
         [Route("get")]
-        [Authorize]
-        public IHttpActionResult Get()
+        [AdminAuthorize]
+        public async Task<IHttpActionResult> Get([FromUri] UserGetModel requestModel)
         {
-            var user = ApplicationContext.Current.CurrentUser;
-            return Json(new {user.Email});
+            //do we have valid values?
+            if (requestModel.Page <= 0)
+                requestModel.Page = 1;
+
+            //do the needful
+            var users = await _userService.GetAsync(x =>
+                (!string.IsNullOrEmpty(requestModel.SearchText) && 
+                (x.FirstName.StartsWith(requestModel.SearchText) ||
+                x.LastName.StartsWith(requestModel.SearchText) ||
+                x.Email == requestModel.SearchText)) || true, null, true, requestModel.Page, requestModel.Count);
+
+            var model = users.ToList().Select(user => user.ToModel());
+            return RespondSuccess(new
+            {
+                Users = model
+            });
         }
     }
 }
---
+++ Presentation/mobSocial.WebApi/Models/Users/UserGetModel.cs
@@ -0,0 +1,14 @@
+﻿using mobSocial.WebApi.Configuration.Mvc.Models;
+
+namespace mobSocial.WebApi.Models.Users
+{
+    public class UserGetModel : RootModel
+    {
+        public string SearchText { get; set; }
+
+        public int Page { get; set; }
+
+        public int Count { get; set; }
+
+    }
+}
