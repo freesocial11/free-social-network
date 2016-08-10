e50592e30fd33f66d67424fa6c627ad7836ab88c
anshul <anshul@apexol.com>
2016-08-10 20:28:39 +0530

    Admin authorize attribute added for admin authorized requests

---
+++ Presentation/mobSocial.WebApi/Configuration/Security/Attributes/AdminAuthorizeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using mobSocial.Services.Extensions;
+using mobSocial.WebApi.Configuration.Infrastructure;
+
+namespace mobSocial.WebApi.Configuration.Security.Attributes
+{
+    /// <summary>
+    /// Specifies tha logged in user must be administrator to access this area
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
+    public class AdminAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (!ApplicationContext.Current.CurrentUser.IsAdministrator())
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorized access");
+            }
+            else
+            {
+                base.OnAuthorization(actionContext);
+            }
+        }
+    }
+}
