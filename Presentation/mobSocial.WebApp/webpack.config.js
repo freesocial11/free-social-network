d93e93ff06d31ad8cda62d681436d31dad4aa930
anshul <anshul@apexol.com>
2016-08-17 12:53:35 +0530

    bundle updated

--- Presentation/mobSocial.WebApp/webpack.config.js
+++ Presentation/mobSocial.WebApp/webpack.config.js
@@ -2,16 +2,22 @@
 "use strict";
 
 module.exports = {
+    externals: {
+        'angular': 'angular'
+    },
     entry: [
             "./libraries/angular/angular.min.js",
             "./libraries/angular/angular-ui-router.js",
             "./libraries/angular/angular-local-storage.min.js",
+            "./libraries/angular/angular-file-upload.min.js",
             "./config.js",
             "./app/app.js",
             "./app/common/router.js",
             "./app/common/authProvider.js",
             "./app/common/requestInterceptor.js",
             "./app/common/webClientService.js",
+            //directives
+            "./app/common/directives/fileUploadButton.js",
             //login
             "./app/public/authentication/loginService.js",
             "./app/public/authentication/loginController.js",
