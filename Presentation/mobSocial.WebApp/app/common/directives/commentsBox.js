169426600870c1bde964c845d10dbd901d68e133
anshul <anshul@apexol.com>
2016-09-03 14:20:59 +0530

    commentbox directive improved for delay loading

--- Presentation/mobSocial.WebApp/app/common/directives/commentsBox.js
+++ Presentation/mobSocial.WebApp/app/common/directives/commentsBox.js
@@ -1,36 +1,26 @@
 ﻿window.mobSocial.lazy.directive("commentsBox", ['commentService', '$rootScope', function (commentService, $rootScope) {
-    return{
+    return {
         restrict: "E",
         templateUrl: "/pages/components/commentsBox.html",
-        replace:true,
+        replace: true,
         scope: {
             EntityName: "@entityname",
             EntityId: "@entityid",
             TotalComments: "@totalcomments"
         },
         link: function ($scope, elem, attr) {
-            $scope.Comment = {
-                CommentText: "",
-                EntityName: $scope.EntityName,
-                EntityId: $scope.EntityId
-            };
-            $scope.CommentRequest = {
-                Page: 0,
-                EntityName: $scope.EntityName,
-                EntityId: $scope.EntityId,
-                Count : $scope.SinglePageCommentCount
-            }
-            $scope.CommentList = [];
+
+
             $scope.CurrentUser = $rootScope.CurrentUser;
             $scope.Post = function () {
-              
+
                 commentService.Post($scope.Comment, function (response) {
                     if (response.Success) {
                         $scope.CommentList.push(response.Comment);
                         $scope.TotalComments++;
                         $scope.Comment.CommentText = "";
                     }
-                }, function() {
+                }, function () {
                     alert("An error occured while performing operation");
                 });
             }
@@ -38,25 +28,27 @@
             $scope.Get = function () {
                 $scope.CommentRequest.Page++;
                 $scope.CommentsLoading = true;
-                commentService.Get($scope.CommentRequest, function (response) {
-                    $scope.CommentsLoading = false;
-                    if (response.Success) {
-                        if (response.Comments.length == 0) {
-                            $scope.NoMoreComments = true;
+                commentService.Get($scope.CommentRequest,
+                    function (response) {
+                        $scope.CommentsLoading = false;
+                        if (response.Success) {
+                            if (response.Comments.length == 0) {
+                                $scope.NoMoreComments = true;
+                            }
+                            $scope.CommentList = $scope.CommentList.concat(response.Comments);
                         }
-                        $scope.CommentList = $scope.CommentList.concat(response.Comments);
-                    }
-                }, function () {
-                    $scope.CommentsLoading = false;
-                    alert("An error occured while performing operation");
-                });
+                    },
+                    function () {
+                        $scope.CommentsLoading = false;
+                        alert("An error occured while performing operation");
+                    });
             }
 
             $scope.Delete = function (id) {
                 if (!confirm("Are you sure you wish to delete this comment?")) {
                     return;
                 }
-                commentService.Delete(id, function(response) {
+                commentService.Delete(id, function (response) {
                     //if the operation succeeds, we'll need to remove the appropriate comment from the list
                     if (response.Success) {
                         for (var i = 0; i < $scope.CommentList.length; i++) {
@@ -68,12 +60,40 @@
                             }
                         }
                     }
-                }, function(response) {
+                }, function (response) {
 
                 });
             }
 
-          
+            $scope.reset = function () {
+                $scope.Comment = {
+                    CommentText: "",
+                    EntityName: $scope.EntityName,
+                    EntityId: $scope.EntityId
+                };
+                $scope.commmentsVisible = false;
+                $scope.CommentRequest = {
+                    Page: 0,
+                    EntityName: $scope.EntityName,
+                    EntityId: $scope.EntityId,
+                    Count: $scope.SinglePageCommentCount
+                }
+                $scope.CommentList = [];
+                $scope.toggleComments();
+            };
+
+            $scope.toggleComments = function () {
+                if ($scope.CommentRequest.Page == 0) {
+                    $scope.Get();
+                    $scope.commmentsVisible = false;
+                }
+                $scope.commmentsVisible = !$scope.commmentsVisible;
+            }
+
+            $scope.$watch("EntityId",
+               function () {
+                   $scope.reset();
+               });
         }
 
     }
