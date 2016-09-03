239d27b2e5d1bf92669dcba8a8a5b6918ad02475
anshul <anshul@apexol.com>
2016-09-03 18:02:44 +0530

    Removed unnecessary code

--- Presentation/mobSocial.WebApp/app/common/directives/mediaButton.js
+++ Presentation/mobSocial.WebApp/app/common/directives/mediaButton.js
@@ -1,4 +1,4 @@
-﻿window.mobSocial.lazy.directive("mediaButton", ["mediaService", "$timeout", function (mediaService, $timeout) {
+﻿window.mobSocial.lazy.directive("mediaButton", [function () {
     return {
         restrict: "A",
         scope: {
@@ -8,29 +8,9 @@
         link: function (scope, elem, attr) {
             elem.bind("click", function () {
                 var modalScope = angular.element(scope.target).scope();
-              /*  const applyMedia = function () {
-                    //safe apply
-                    $timeout(function () {
-                        modalScope.media = scope.media;
-                    }, 0);
-                } */
-
                 if (!scope.media.FullyLoaded) {
                     modalScope.reloadMedia(scope.media.Id);
-                   /* mediaService.get(scope.media.Id,
-                        function(response) {
-                            if (response.Success)
-                                scope.media = response.ResponseData.Media;
-                            applyMedia();
-                        },
-                        function(response) {
-
-                        }); */
-                } else {
-                   // applyMedia();
-                }
-                
-
+                } 
                 jQuery(scope.target).modal();
             });
         }
