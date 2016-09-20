e38bec8a76132e5aed8cc69314dc22f3230f87e8
anshul <anshul@apexol.com>
2016-09-20 19:15:07 +0530

    Email scheduler task added

---
+++ Presentation/mobSocial.WebApi/Configuration/Tasks/EmailSchedulerTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using mobSocial.Core.Infrastructure.AppEngine;
+using mobSocial.Core.Tasks;
+using mobSocial.Services.Emails;
+
+namespace mobSocial.WebApi.Configuration.Tasks
+{
+    public class EmailSchedulerTask : ITask
+    {
+        public void Dispose()
+        {
+            //do nothing else
+        }
+
+        public void Run()
+        {
+            //resolve email sender service
+            var emailService = mobSocialEngine.ActiveEngine.Resolve<IEmailService>();
+            var emailMessages = emailService.Get(x => !x.IsSent && x.SendingDate <= DateTime.UtcNow).ToList();
+
+            foreach (var message in emailMessages)
+                emailService.SendEmail(message);
+        }
+
+        public string SystemName => "mobSocial.WebApi.Configuration.Tasks.EmailSchedulerTask";
+    }
+}
