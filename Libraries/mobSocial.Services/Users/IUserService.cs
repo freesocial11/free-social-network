15c174385bee1d604c6800cbb826328ce24dfc53
anshul <anshul@apexol.com>
2016-11-16 19:50:51 +0530

    additional search methods added in user service

--- Libraries/mobSocial.Services/Users/IUserService.cs
+++ Libraries/mobSocial.Services/Users/IUserService.cs
@@ -7,5 +7,9 @@ namespace mobSocial.Services.Users
     public interface IUserService : IBaseEntityService<User>
     {
         IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, int page, int count);
+
+        IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, string[] restrictToRoles, int page, int count);
+
+        IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, int[] restrictToRoles, int page, int count);
     }
 }
--- Libraries/mobSocial.Services/Users/UserService.cs
+++ Libraries/mobSocial.Services/Users/UserService.cs
@@ -8,15 +8,32 @@ namespace mobSocial.Services.Users
 {
     public class UserService : MobSocialEntityService<User>, IUserService
     {
-        public UserService(IDataRepository<User> dataRepository) : base(dataRepository)
+        private readonly IDataRepository<Role> _roleRepository;
+        public UserService(IDataRepository<User> dataRepository, IDataRepository<Role> roleRepository) : base(dataRepository)
         {
+            _roleRepository = roleRepository;
         }
 
         public IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, int page, int count)
         {
-            return Get(x => (x.FirstName.Contains(searchText) || x.LastName.Contains(searchText) || x.Email.Contains(searchText)) 
-            && (!excludeLoggedInUser || x.IsRegistered()), x => x.FirstName, true, page,
+            return Get(x => (x.FirstName.Contains(searchText) || x.LastName.Contains(searchText) || x.Email.Contains(searchText)), x => x.FirstName, true, page,
                 count).ToList();
+        }
+
+        public IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, string[] restrictToRoles, int page, int count)
+        {
+            var roleIds = _roleRepository.Get(x => restrictToRoles.Contains(x.SystemName)).Select(x => x.Id).ToArray();
+            return SearchUsers(searchText, excludeLoggedInUser, roleIds, page, count);
+        }
+
+        public IList<User> SearchUsers(string searchText, bool excludeLoggedInUser, int[] restrictToRoles, int page, int count)
+        {
+            var query = Get(
+                x =>
+                    (x.FirstName.Contains(searchText) || x.LastName.Contains(searchText) || x.Email.Contains(searchText)) &&
+                    restrictToRoles.Intersect(x.UserRoles.Select(y => y.RoleId)).Any(), x => x.FirstName, true, page,
+                count);
+            return query.ToList();
         }
     }
 }
