049e8a06449cf5561b31b611e4a65e663bd4db6e
anshul <anshul@apexol.com>
2016-12-13 15:48:33 +0530

    Notification hub for server communication

---
+++ Presentation/mobSocial.WebApi/Configuration/SignalR/Hubs/NotificationHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+
+namespace mobSocial.WebApi.Configuration.SignalR.Hubs
+{
+    [HubName("notification")]
+    public class NotificationHub : MobSocialHub
+    {
+    }
+}
