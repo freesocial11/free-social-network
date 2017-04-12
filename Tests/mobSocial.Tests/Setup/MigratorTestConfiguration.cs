849c8c38293e4f9cee03e621909d077dcf9f7369
anshul <anshul@apexol.com>
2017-04-12 18:06:49 +0530

    removed header comment

--- Tests/mobSocial.Tests/Setup/MigratorTestConfiguration.cs
+++ Tests/mobSocial.Tests/Setup/MigratorTestConfiguration.cs
@@ -1,11 +1,4 @@
-﻿// #region Author Information
-// // MigratorTestConfiguration.cs
-// // 
-// // (c) 2017 Apexol Technologies. All Rights Reserved.
-// // 
-// #endregion
-
-using System.Data.Entity.Infrastructure;
+﻿using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Migrations;
 using mobSocial.Data.Database;
 
