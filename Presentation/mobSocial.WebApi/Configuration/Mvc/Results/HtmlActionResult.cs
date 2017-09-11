1ef7678169cdea84bba0440ac63b6d622afe6449
anshul <anshul@apexol.com>
2017-09-11 17:17:59 +0530

    missed file to commit 0f4ec7c

---
+++ Presentation/mobSocial.WebApi/Configuration/Mvc/Results/HtmlActionResult.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using mobSocial.WebApi.Configuration.Mvc.UI;
+
+namespace mobSocial.WebApi.Configuration.Mvc.Results
+{
+    public class HtmlActionResult : IHttpActionResult
+    {
+        private const string ViewDirectory = @"~/Views/";
+        private readonly string _viewName;
+        private readonly object _model;
+
+        public HtmlActionResult(string viewName, object model = null)
+        {
+            _viewName = ViewDirectory + viewName + ".cshtml";
+            _model = model;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            var content = ViewRenderer.RenderView(_viewName, _model);
+            response.Content = new StringContent(content);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return Task.FromResult(response);
+        }
+    }
+}
